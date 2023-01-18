6b575618622c5ad3d8f5642d954e349754790c61
hkl <hkl@local>
2023-01-18 21:36:37 +0500

    Removed comments. Minor changes.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,27 +3,33 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-//import jm.task.core.jdbc.util.Util;
 
-
-/*import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;*/
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-
+        User man;
         UserService userService = new UserServiceImpl();
+
         userService.createUsersTable();
 
-        userService.saveUser("Ivan", "Ivanov", (byte) 11);
-        userService.saveUser("Petr", "Petrov", (byte) 22);
-        userService.saveUser("Sidor", "Sidorov", (byte) 33);
-        userService.saveUser("Незнайка", "Незнанский", (byte) 44);// Have "?" with cyrillic
+        man = new User("Ivan", "Ivanov", (byte) 11);
+        userService.saveUser(man.getName(), man.getLastName(), man.getAge());
+        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+
+        man = new User("Petr", "Petrov", (byte) 22);
+        userService.saveUser(man.getName(), man.getLastName(), man.getAge());
+        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+
+        man = new User("Sidor", "Sidorov", (byte) 33);
+        userService.saveUser(man.getName(), man.getLastName(), man.getAge());
+        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+
+        man = new User("Незнайка", "Незнанский", (byte) 44);
+        userService.saveUser(man.getName(), man.getLastName(), man.getAge());
+        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
 
         List<User> userList = userService.getAllUsers();
         StringBuilder sb = new StringBuilder();
@@ -32,38 +38,9 @@ public class Main {
                     .append(user.getName()).append(" ")
                     .append(user.getLastName()).append(" ")
                     .append(user.getAge()).append("\n");
-            //System.out.println(user.getId() + user.getName() + user.getLastName() + user.getAge());
         }
         System.out.println(sb);
 
         userService.dropUsersTable();
-
-/*        try (Connection myConn = Util.getConnection();
-                Statement statement = myConn.createStatement()) {
-            //CREATE TABLE IF NOT EXISTS users (id BIGINT primary key auto_increment, name VARCHAR(10), lastname VARCHAR(10), age TINYINT)
-
-
-            //statement.executeUpdate("CREATE TABLE User (firstname VARCHAR(12), secondname VARCHAR(12))");
-            //statement.executeUpdate("INSERT INTO User ('Ivan', 'Ivanov')");
-            statement.executeUpdate("INSERT INTO User (name, lastName) VALUES ('Ivan', 'Ivanov')");
-            //statement.executeUpdate("INSERT INTO User ('Petr', 'Petrov')");
-            statement.executeUpdate("INSERT INTO User (name, lastName) VALUES ('Petr', 'Petrov')");
-            //statement.executeUpdate("INSERT INTO User ('Sidor', 'Sidorov')");
-            statement.executeUpdate("INSERT INTO User (name, lastName) VALUES ('Sidor', 'Sidorov')");
-            //statement.executeUpdate("INSERT INTO User ('Незнайка', 'Незнанский')");
-            statement.executeUpdate("INSERT INTO User (name, lastName) VALUES ('Незнайка', 'Незнанский')");
-            //statement.executeUpdate("INSERT INTO User (firstname, secondname) VALUES ('Ivan', 'Ivanov')");
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM User");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
-            }
-            statement.executeUpdate("DROP TABLE User");
-            //statement.
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,124 +1,38 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+//import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 //import jm.task.core.jdbc.util.Util;
 
-/*import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;*/
 import java.util.List;
 
-/**
- * Комментарии оставлены в целях археологии
- * */
 public class UserServiceImpl implements UserService {
-    //private User user = new User();
-/*    private static final String tableName = User.class.getSimpleName();
-    private static final Field[] userFields = User.class.getDeclaredFields();// .class.getFields();
-    //private static final String idField = User.class.getFields()[0].getName();
-    private static final String idField = userFields[0].getName();
-    private static final String nameField = userFields[1].getName();
-    private static final String lastnameField = userFields[2].getName();
-    private static final String ageField = userFields[3].getName();*/
-
-    private final UserDao userDao = new UserDaoJDBCImpl();
+    //private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
-/*        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement()) {
-            String str = "CREATE TABLE IF NOT EXISTS " + tableName +
-                    " (" +
-                    idField + " BIGINT primary key auto_increment, " +
-                    nameField + " VARCHAR(10), " +
-                    lastnameField + " VARCHAR(10), " +
-                    ageField + " TINYINT)";
-            statement.executeUpdate(str);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 
     public void dropUsersTable() {
         userDao.dropUsersTable();
-/*        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS " + tableName);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
-/*        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement()) {
-            String str = "INSERT INTO " + tableName +
-                    " (" + nameField + "," + lastnameField + "," + ageField + ") " +
-                    "VALUES (\'" + name + "\',\'" + lastName + "\'," + age + ")";
-            statement.executeUpdate(str);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 
     public void removeUserById(long id) {
         userDao.removeUserById(id);
-/*        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement()) {
-            statement.executeUpdate("DELETE FROM " + tableName + " WHERE " + tableName + "." + idField + "=" + id);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
-/*        List<User> userList = new ArrayList<>();
-
-        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName)) {
-            while (resultSet.next()) {
-                userList.add(new User(
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        Byte.parseByte(resultSet.getString(4))));
-            };
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return userList;*/
     }
 
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
-/*        try(Connection conn = Util.getConnection();
-                Statement statement = conn.createStatement()) {
-
-            //statement.executeUpdate("DELETE FROM " + tableName);
-            statement.executeUpdate("TRUNCATE TABLE " + tableName);
-            //statement.execute("DELETE FROM " + tableName);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
     }
 }
